9a3efeb9610d2a823ed2e04c848578fd6f78efe2
Bennie Mosher <benniemosher@gmail.com>
2020-04-04 04:52:14 -0600

    Tailwindcss (#3)
    * Ran yarn add tailwindcss --dev
    
    * Write configs for tailwindcss
    
    * Start to style layout
    
    * Generate devise views so we can tailwind them
    
    * Style login page
    
    * Style sign up page
    
    * Style forgot password page
    
    * Style basic home page
    
    * Switch to using webpacker for tailwind
    
    * Update postcss
    
    * Oddball it up!

---
+++ app/javascript/css/tailwind.js
@@ -0,0 +1,20 @@
+module.exports = {
+  theme: {
+    boxShadow: {
+      outline: "0 0 0 3px rgba(0, 161, 135 ,0.5)"
+    },
+    container: {
+      center: true
+    },
+    extend: {
+      colors: {
+        oddball: {
+          default: "#00a187",
+          lighter: "#00c3a3"
+        }
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+};
