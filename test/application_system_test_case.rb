9210d4601935849a48ddcd54d411054f09778977
Bennie Mosher <benniemosher@gmail.com>
2020-04-07 16:15:52 -0600

    First attempt at tests (#5)
    * First attempt at tests
    
    * Setup factory girl
    
    * Test login
    
    * More tests
    
    * Add confirmation password test
    
    * Forgot password test
    
    * Add browser to  image name
    
    * Switch to rake
    
    * Fix typo
    
    * Add system tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,10 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  # driven_by :selenium, using: :firefox, screen_size: [1400, 1400]
+  driven_by :selenium, using: :headless_firefox
+
+  include Devise::Test::IntegrationHelpers
 end
