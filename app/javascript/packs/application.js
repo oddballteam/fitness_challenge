610f137ba04101e0a53fcb984868f5d6801fb97f
Bennie Mosher <benniemosher@gmail.com>
2020-04-08 17:53:01 -0600

    Add flatpickr

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,19 +1,18 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
-import Rails from "@rails/ujs";
-Rails.start();
+require("@rails/ujs").start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
 
 import "../css/application";
+
+require("flatpickr/dist/flatpickr.css");
+
 import { Application } from "stimulus";
+import Flatpickr from "stimulus-flatpickr";
 import { definitionsFromContext } from "stimulus/webpack-helpers";
 
 const application = Application.start();
-const controllers = require.context("./controllers", true, /\.js$/);
-
+const controllers = require.context("../controllers", true, /\.js$/);
 application.load(definitionsFromContext(controllers));
+
+application.register("flatpickr", Flatpickr);
