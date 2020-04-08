3599c27df2c601be79ae6283d6bc633df5e27872
Bennie Mosher <benniemosher@gmail.com>
2020-04-08 15:50:32 -0600

    Trying to install flatpickr

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,10 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
-
+require("@rails/ujs").start();
+require("turbolinks").start();
+require("@rails/activestorage").start();
+require("channels");
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,6 +14,17 @@ require("channels")
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-import '../css/application.css'
+import "../css/application.css";
 
-import "controllers"
+import "controllers";
+
+import { Application } from "stimulus";
+import Flatpickr from "stimulus-flatpickr";
+
+import { definitionsFromContext } from "stimulus/webpack-helpers";
+const application = Application.start();
+const context = require.context("../controllers", true, /\.js$/);
+application.load(definitionsFromContext(context));
+
+// Manually register Flatpickr as a stimulus controller
+application.register("flatpickr", Flatpickr);
