41fde8740262e316eb16a69618e51abafba4559b
Bennie Mosher <benniemosher@gmail.com>
2020-04-08 16:57:57 -0600

    check in flatpickr stuff

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,17 +3,17 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-require("@rails/ujs").start();
+import Rails from "@rails/ujs";
+Rails.start();
 require("turbolinks").start();
 require("@rails/activestorage").start();
 require("channels");
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
-import "../css/application.css";
+import "../css/application";
+import { Application } from "stimulus";
+import { definitionsFromContext } from "stimulus/webpack-helpers";
 
-import "controllers";
+const application = Application.start();
+const controllers = require.context("./controllers", true, /\.js$/);
+
+application.load(definitionsFromContext(controllers));
