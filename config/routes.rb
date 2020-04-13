89f6afb284a6a9a406c721995b87de6f8de89573
Bennie Mosher <benniemosher@gmail.com>
2020-04-13 11:57:57 -0600

    Point devise to omniauth callbacks controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'omniauth_callbacks' }
   root to: 'home#index'
 
   resources :challenges
