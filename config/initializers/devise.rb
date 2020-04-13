ed7387e045d8a2b699193e93e3290d63618d1597
Bennie Mosher <benniemosher@gmail.com>
2020-04-13 11:58:42 -0600

    Add omniauth to config

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -25,4 +25,6 @@ Devise.setup do |config|
   config.reset_password_within = 6.hours
 
   config.sign_out_via = :delete
+
+  config.omniauth :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], access_type: 'online'
 end
