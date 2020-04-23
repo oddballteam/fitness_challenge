bdc1cc7f181367f0e16ce8aca0e72f30d6176954
Bennie Mosher <benniemosher@gmail.com>
2020-04-23 12:25:04 -0600

    GH-25: Fix between check

--- app/models/challenge_entry.rb
+++ app/models/challenge_entry.rb
@@ -4,10 +4,7 @@ class ChallengeEntry < ApplicationRecord
   validates_presence_of :amount
   validates_presence_of :completed_date
 
-  validates_date :completed_date, between: [
-    challenge.start_date,
-    challenge.end_date
-  ]
+  validates_date :completed_date, between: %i[start_date end_date]
 
   belongs_to :user
   belongs_to :challenge
