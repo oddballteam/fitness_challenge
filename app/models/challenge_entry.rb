5d472bb588df6d8530f49f2f51ad82de23cb2373
Bennie Mosher <bennie.mosher@oddball.io>
2020-04-24 10:59:45 -0600

    GH-25: Cannot add entry outside of challenge dates (#29)
    * GH-25: Cannot add entry outside of challenge dates
    
    * GH-25: check-in model validation to receive help
    
    * GH-25: Fix between check
    
    * GH-25: Update factory
    
    * GH-25: Properly validate completed date
    
    * GH-25: Clean up spec
    
    * GH-25: Give people a buffer to put in entries

--- app/models/challenge_entry.rb
+++ app/models/challenge_entry.rb
@@ -3,7 +3,18 @@
 class ChallengeEntry < ApplicationRecord
   validates_presence_of :amount
   validates_presence_of :completed_date
+  validate :validate_completed_date
 
   belongs_to :user
   belongs_to :challenge
+
+  private
+
+  def validate_completed_date
+    return if challenge.nil?
+    return if completed_date > challenge.start_date
+    return if completed_date < challenge.end_date
+
+    errors.add :completed_date, :should_be_during_challenge_dates
+  end
 end
--- spec/system/challenge_entries_system_spec.rb
+++ spec/system/challenge_entries_system_spec.rb
@@ -4,64 +4,117 @@ require 'rails_helper'
 
 RSpec.describe 'ChallengeEntries', type: :system do
   let(:user) { FactoryBot.create(:user) }
-  let!(:challenge) { FactoryBot.create(:challenge, user: user) }
 
-  context 'new' do
-    let(:entry) do
-      FactoryBot.build(:challenge_entry, challenge: challenge, user: user)
+  context 'challenge not started' do
+    let!(:challenge) do
+      FactoryBot.create(
+        :challenge,
+        user: user,
+        start_date: (Date.today + 3),
+        end_date: (Date.today + 6)
+      )
     end
 
-    before(:each) do
-      login_as(user, scope: :user, run_callbacks: false)
-      visit root_url
-    end
+    context 'new' do
+      before(:each) do
+        login_as(user, scope: :user, run_callbacks: false)
+        visit root_url
+      end
 
-    it 'add' do
-      click_on challenge.name
+      it 'can not add an entry' do
+        click_on challenge.name
 
-      first(:link, 'Add Entry').click
-
-      fill_in I18n.t('helpers.label.challenge_entry.amount'),
-              with: entry.amount
-      fill_in I18n.t('helpers.label.challenge_entry.completed_date'),
-              with: entry.completed_date
-      click_on I18n.t('helpers.submit.challenge_entry.create')
-
-      expect(page).to have_text entry.amount
-      expect(page).to have_text entry.completed_date
+        expect(page).to_not have_content('Add Entry')
+      end
     end
   end
 
-  context 'exists' do
-    let(:new_amount) { rand(100) }
-    let!(:entry) do
-      FactoryBot.create(:challenge_entry, challenge: challenge, user: user)
+  context 'challenge ended' do
+    let!(:challenge) do
+      FactoryBot.create(
+        :challenge,
+        user: user,
+        start_date: (Date.today - 6),
+        end_date: (Date.today - 1)
+      )
     end
 
-    before(:each) do
-      login_as(user, scope: :user, run_callbacks: false)
-      visit challenge_path(challenge)
-    end
-
-    it 'edit' do
-      within "#entry-#{entry.id}" do
-        click_on I18n.t('helpers.actions.edit')
+    context 'new' do
+      before(:each) do
+        login_as(user, scope: :user, run_callbacks: false)
+        visit root_url
       end
 
-      fill_in I18n.t('helpers.label.challenge_entry.amount'), with: new_amount
-      click_on I18n.t('helpers.submit.challenge_entry.update')
+      it 'can not add an entry' do
+        click_on challenge.name
 
-      within "#entry-#{entry.id}" do
-        expect(page).to have_text new_amount
+        expect(page).to_not have_content('Add Entry')
       end
     end
+  end
 
-    it 'delete' do
-      within "#entry-#{entry.id}" do
-        click_on I18n.t('helpers.actions.delete')
+  context 'challenge started' do
+    let!(:challenge) { FactoryBot.create(:challenge, user: user) }
+
+    context 'new' do
+      let(:entry) do
+        FactoryBot.build(:challenge_entry, challenge: challenge, user: user)
       end
 
-      expect(page).to_not have_selector "#entry-#{entry.id}"
+      before(:each) do
+        login_as(user, scope: :user, run_callbacks: false)
+        visit root_url
+      end
+
+      it 'add' do
+        click_on challenge.name
+
+        first(:link, 'Add Entry').click
+
+        fill_in I18n.t('helpers.label.challenge_entry.amount'),
+                with: entry.amount
+        fill_in I18n.t('helpers.label.challenge_entry.completed_date'),
+                with: entry.completed_date
+        click_on I18n.t('helpers.submit.challenge_entry.create')
+
+        expect(page).to have_text entry.amount
+        expect(page).to have_text entry.completed_date
+      end
+    end
+
+    context 'exists' do
+      let(:new_amount) { rand(100) }
+      let!(:entry) do
+        FactoryBot.create(:challenge_entry,
+                          challenge: challenge,
+                          user: user)
+      end
+
+      before(:each) do
+        login_as(user, scope: :user, run_callbacks: false)
+        visit challenge_path(challenge)
+      end
+
+      it 'edit' do
+        within "#entry-#{entry.id}" do
+          click_on I18n.t('helpers.actions.edit')
+        end
+
+        fill_in I18n.t('helpers.label.challenge_entry.amount'), with: new_amount
+        click_on I18n.t('helpers.submit.challenge_entry.update')
+
+        within "#entry-#{entry.id}" do
+          expect(page).to have_text new_amount
+        end
+      end
+
+      it 'delete' do
+        within "#entry-#{entry.id}" do
+          click_on I18n.t('helpers.actions.delete')
+        end
+
+        expect(page).to_not have_selector "#entry-#{entry.id}"
+      end
     end
   end
 end
