150c0a7681987322f856120ea7824b4bafac0656
Bennie Mosher <benniemosher@gmail.com>
2020-04-24 09:43:34 -0600

    GH-25: Properly validate completed date

--- app/models/challenge_entry.rb
+++ app/models/challenge_entry.rb
@@ -3,9 +3,18 @@
 class ChallengeEntry < ApplicationRecord
   validates_presence_of :amount
   validates_presence_of :completed_date
-
-  validates_date :completed_date, between: %i[start_date end_date]
+  validate :validate_completed_date
 
   belongs_to :user
   belongs_to :challenge
+
+  private
+
+  def validate_completed_date
+    return if challenge.nil?
+    return if completed_date > challenge.start_date
+    return if completed_date < challenge.end_date
+
+    errors.add :completed_date, :should_be_during_challenge_dates
+  end
 end
