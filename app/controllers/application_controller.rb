b1b4cbdc742b9f35e2acefa201d77acbb7ba1b93
Bennie Mosher <benniemosher@gmail.com>
2020-05-07 12:19:52 -0600

    Update applicatino contorller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   add_flash_types :success, :error
+  skip_before_action :verify_authenticity_token, if: :json_request?
+  respond_to :html, :json
+
+  private
+
+  def json_request?
+    request.format.json?
+  end
 end
